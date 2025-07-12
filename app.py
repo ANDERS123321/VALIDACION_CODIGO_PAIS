b93fecf53afd5b08e5a3da33e01139d461512133
Alessandrou27 <mariachavezm9@gmail.com>
2025-07-12 13:40:07 -0500

    add creacion archivo principal python

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify, send_from_directory
+from src.validador import validar_telefono
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/validador', methods=['POST'])
+def validar():
+    datos = request.get_json()
+    numero = datos.get("numero", "")
+
+    if validar_telefono(numero):
+        return jsonify({"numero": numero, "valido": True, "mensaje": "Número Valido"})
+    else:
+        return jsonify({"numero": numero, "valido": False, "mensaje": "Número Inválido"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
